c8c29d74ad965cb07f50fc5a7bc833dbb82ac7c5
Ali Seraji <aseraji2012@gmail.com>
2025-12-17 04:58:47 +0900

    initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Link, redirect } from 'react-router';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'The Rock accounting' },
+    { name: 'THE ROCK', content: 'Welcome to The Rock' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-[#FFF]">
+      <p className="text-black mb-2.5">
+        Welcome to the Rock accounting procedure
+      </p>
+      <Link to={`/rock`}>
+        <button className="flex flex-row h-10 w-48 bg-[blue] text-[16px] rounded-sm text-[white] justify-center items-center hover:cursor-pointer hover:bg-blue-500 ">
+          {`Let's see the next page`}
+        </button>
+      </Link>
+    </div>
+  );
 }
---
+++ app/routes/receipts/RockReceipt.tsx
@@ -0,0 +1,5 @@
+export default function RockReceipt() {
+  return (
+    <div className="flex flex-row justify-center items-center text-[#FAA6]">{`Now the rock page`}</div>
+  );
+}
