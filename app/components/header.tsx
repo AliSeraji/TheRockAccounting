eb1d0a9742e3df92010431175f5b21293d4e37e1
Alpha <aseraji2012@gmail.com>
2026-09-10 21:41:02 +0330

    fixed naming

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
       <div className="max-w-full px-4 py-4">
         <div className="flex flex-row-reverse items-center justify-between">
           <h1 className="text-2xl font-bold bg-linear-to-r from-teal-600 to-blue-600 bg-clip-text text-transparent">
-            سیستم یکپارچه شرکت
+            نرم افزار مدریت و فروش سنگ
           </h1>
-          <div className="text-sm text-gray-600">(سنگ احسان و رفقا)</div>
+          <div className="text-sm text-gray-600">(شرکت سنگ .....)</div>
         </div>
       </div>
     </header>
